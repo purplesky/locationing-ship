11ca891a6befded2e498ecaf6052ca0480686efc
Nguyễn Bá Chí Công <nbccong@inetcloud.vn>
2013-06-08 22:36:59 +0700

    update connection database

--- core/include/connect.php
+++ core/include/connect.php
@@ -1,8 +1,8 @@
 <?php
-  $myhost = "localhost";
-  $taikhoan = "root";
-  $matkhau = "";
-  $cosodulieu = "dvtb";
-  mysql_connect($myhost, $taikhoan, $matkhau) or die("Khong the ket noi den CSDL ");
-  mysql_select_db($cosodulieu) or die("Khong tim thay <b>$cosodulieu</b> tren localhost");
+  $host = "localhost";
+  $user = "root";
+  $pass = "";
+  $data = "dvtb";
+  mysql_connect($host, $user, $pass) or die("Can't connect Database!");
+  mysql_select_db($data) or die("Not found Database <b>$data</b>!");
 ?>
